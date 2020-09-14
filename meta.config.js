2e1ac6e9c133cad2cc580e99dff6ff0047519667
xusenyue <xusenyue@111.com.cn>
2020-09-14 23:00:13 +0800

    update

--- meta.config.js
+++ meta.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  name: {
-    type: "input",
-    message: "项目名称：",
-  },
-  version: {
-    type: "input",
-    message: "项目版本：",
-    default: "1.0.0",
-  },
-  desc: {
-    type: "input",
-    message: "项目描述：",
-    default: "A test project",
+  prompts: {
+    name: {
+      type: "input",
+      message: "项目名称：",
+    },
+    version: {
+      type: "input",
+      message: "项目版本：",
+      default: "1.0.0",
+    },
+    desc: {
+      type: "input",
+      message: "项目描述：",
+      default: "A test project",
+    },
   },
 };
